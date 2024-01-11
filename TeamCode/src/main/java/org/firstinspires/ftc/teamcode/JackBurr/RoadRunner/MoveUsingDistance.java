d0ea396a1b0b1d1b97e608c6c850eb1786538cb1
Fenwick <BurrJ22@outlook.com>
2024-01-11 17:06:10 -0500

    Added new files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JackBurr/RoadRunner/MoveUsingDistance.java
@@ -0,0 +1,66 @@
+package org.firstinspires.ftc.teamcode.JackBurr.RoadRunner;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+public class MoveUsingDistance extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+    public Trajectory getNewLeftTrajectory(double distance, HardwareMap hardwareMap){
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        Trajectory new_trajectory = drive.trajectoryBuilder(new Pose2d())
+                .strafeLeft(distance)
+                .build();
+        return new_trajectory;
+    }
+    public Trajectory getNewRightTrajectory(double distance, HardwareMap hardwareMap){
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        Trajectory new_trajectory = drive.trajectoryBuilder(new Pose2d())
+                .strafeRight(distance)
+                .build();
+        return new_trajectory;
+    }
+    public Trajectory getNewForwardTrajectory(double distance, HardwareMap hardwareMap){
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        Trajectory new_trajectory = drive.trajectoryBuilder(new Pose2d())
+                .forward(distance)
+                .build();
+        return new_trajectory;
+    }
+    public Trajectory getNewBackwardTrajectory(double distance, HardwareMap hardwareMap){
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        Trajectory new_trajectory = drive.trajectoryBuilder(new Pose2d())
+                .back(distance)
+                .build();
+        return new_trajectory;
+    }
+    public void moveLeft(double distance, HardwareMap hardwareMap){
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        drive.followTrajectory(getNewLeftTrajectory(distance, hardwareMap));
+    }
+    public void moveRight(double distance, HardwareMap hardwareMap){
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        drive.followTrajectory(getNewRightTrajectory(distance, hardwareMap));
+    }
+    public void moveForward(double distance, HardwareMap hardwareMap){
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        drive.followTrajectory(getNewForwardTrajectory(distance, hardwareMap));
+    }
+    public void moveBackward(double distance, HardwareMap hardwareMap){
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        drive.followTrajectory(getNewBackwardTrajectory(distance, hardwareMap));
+    }
+
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JackBurr/RoadRunner/RoadRunnerTest01.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.JackBurr.RoadRunner;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class RoadRunnerTest01 extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}

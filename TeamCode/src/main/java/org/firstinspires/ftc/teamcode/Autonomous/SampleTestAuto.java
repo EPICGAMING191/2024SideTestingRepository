5eac8fc440be1d6646037478fd7ff869feeca07e
Fenwick <BurrJ22@outlook.com>
2024-01-11 16:48:01 -0500

    Added new folders and files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/SampleTestAuto.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@Autonomous
+public class SampleTestAuto extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void init_loop(){
+
+    }
+
+    @Override
+    public void start(){
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}

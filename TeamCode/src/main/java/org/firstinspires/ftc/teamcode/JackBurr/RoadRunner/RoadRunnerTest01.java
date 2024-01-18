f11bb80a552152338f21f9030a0063408fb88725
Fenwick <BurrJ22@outlook.com>
2024-01-18 16:22:37 -0500

    Updated RoadRunner test files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JackBurr/RoadRunner/RoadRunnerTest01.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JackBurr/RoadRunner/RoadRunnerTest01.java
@@ -10,6 +10,7 @@ public class RoadRunnerTest01 extends OpMode {
         MoveUsingDistance distance = new MoveUsingDistance();
         distance.moveForward(24, hardwareMap);
         telemetry.addLine("Robot should have moved forward 24 inches.");
+        telemetry.update();
     }
 
     @Override

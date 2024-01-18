53f6ef8d199d427030ecdb9e1d8fe852144611f7
Fenwick <BurrJ22@outlook.com>
2024-01-18 16:04:00 -0500

    Added new files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JackBurr/RoadRunner/RoadRunnerTest01.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JackBurr/RoadRunner/RoadRunnerTest01.java
@@ -1,10 +1,17 @@
 package org.firstinspires.ftc.teamcode.JackBurr.RoadRunner;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
+@Autonomous
 public class RoadRunnerTest01 extends OpMode {
     @Override
     public void init() {
+
+    }
+
+    @Override
+    public void init_loop(){
 
     }
 

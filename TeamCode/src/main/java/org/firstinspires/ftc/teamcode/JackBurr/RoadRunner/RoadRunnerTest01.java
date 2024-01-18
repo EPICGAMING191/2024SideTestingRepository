5429cc5e9520113b7766deee2e743b7a5d5f7fa1
Fenwick <BurrJ22@outlook.com>
2024-01-18 16:08:57 -0500

    Updated RoadRunner test files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JackBurr/RoadRunner/RoadRunnerTest01.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JackBurr/RoadRunner/RoadRunnerTest01.java
@@ -7,7 +7,9 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 public class RoadRunnerTest01 extends OpMode {
     @Override
     public void init() {
-
+        MoveUsingDistance distance = new MoveUsingDistance();
+        distance.moveForward(24, hardwareMap);
+        telemetry.addLine("Robot should have moved forward 24 inches.");
     }
 
     @Override

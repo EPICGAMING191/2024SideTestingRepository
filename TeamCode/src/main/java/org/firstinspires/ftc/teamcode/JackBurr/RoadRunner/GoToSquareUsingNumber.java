458b71b13ae101a7ba2bf20799dfc267d008b0bd
EPICGAMING191 <112524668+EPICGAMING191@users.noreply.github.com>
2024-03-19 22:45:32 +0000

    Bug fixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JackBurr/RoadRunner/GoToSquareUsingNumber.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JackBurr/RoadRunner/GoToSquareUsingNumber.java
@@ -11,12 +11,27 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 public class GoToSquareUsingNumber extends OpMode {
     //Tiles are 2 ft x 2 ft
     //Board is 6 tiles by 6 tiles
+    enum CurrentRobotOrientation {
+        FORWARD,
+        BACKWARD,
+        LEFT,
+        RIGHT
+    }
+
     MoveUsingDistance moveUsingDistance;
+    CurrentRobotOrientation currentRobotOrientation;
     int currentSquare;
+
     @Override
     public void init() {
         moveUsingDistance = new MoveUsingDistance();
+        currentRobotOrientation = CurrentRobotOrientation.FORWARD;
         currentSquare = 31;
+    }
+
+    @Override 
+    public void init_loop(){
+        //Add adjustments using controller here
     }
 
     @Override
@@ -47,6 +62,9 @@ public class GoToSquareUsingNumber extends OpMode {
     public void move_to_square(int target_square){
         int target_row;
         int current_row;
+        int target_column;
+        int current_column;
+
         if (target_square >= 1 && target_square <= 6){
             target_row = 1;
         }
@@ -92,7 +110,69 @@ public class GoToSquareUsingNumber extends OpMode {
             telemetry.addLine("Invalid current square.");
             requestOpModeStop();
         }
-        move_forward_by_squares(current_row - target_row);
+
+        if (target_square == 1 || target_square == 7 || target_square == 13 || target_square == 19 || target_square == 25 || target_square == 31){
+            target_column = 1;
+        }
+        else if (target_square == 2 || target_square == 8 || target_square == 14 || target_square == 20 || target_square == 26 || target_square == 32){
+            target_column = 2;
+        }
+        else if (target_square == 3 || target_square == 9 || target_square == 15 || target_square == 21 || target_square == 27 || target_square == 33){
+            target_column = 3;
+        }
+        else if (target_square == 4 || target_square == 10 || target_square == 16 || target_square == 22 || target_square == 28 || target_square == 34){
+            target_column = 4;
+        }
+        else if (target_square == 5 || target_square == 11 || target_square == 17 || target_square == 23 || target_square == 29 || target_square == 35){
+            target_column = 5;
+        }
+        else if (target_square == 6 || target_square == 12 || target_square == 18 || target_square == 24 || target_square == 30 || target_square == 36){
+            target_column = 6;
+        }
+        else {
+            telemetry.addLine("Invalid current square.");
+            requestOpModeStop();
+        }
+
+        if (currentSquare == 1 || currentSquare == 7 || currentSquare == 13 || currentSquare == 19 || currentSquare == 25 || currentSquare == 31){
+            current_column = 1;
+        }
+        else if (currentSquare == 2 || currentSquare == 8 || currentSquare == 14 || currentSquare == 20 || currentSquare == 26 || currentSquare == 32){
+            current_column = 2;
+        }
+        else if (currentSquare == 3 || currentSquare == 9 || currentSquare == 15 || currentSquare == 21 || currentSquare == 27 || currentSquare == 33){
+            current_column = 3;
+        }
+        else if (currentSquare == 4 || currentSquare == 10 || currentSquare == 16 || currentSquare == 22 || currentSquare == 28 || currentSquare == 34){
+            current_column = 4;
+        }
+        else if (currentSquare == 5 || currentSquare == 11 || currentSquare == 17 || currentSquare == 23 || currentSquare == 29 || currentSquare == 35){
+            current_column = 5;
+        }
+        else if (currentSquare == 6 || currentSquare == 12 || currentSquare == 18 || currentSquare == 24 || currentSquare == 30 || currentSquare == 36){
+            current_column = 6;
+        }
+        else {
+            telemetry.addLine("Invalid target square.");
+            requestOpModeStop();
+        }
+
+        if (currentRobotOrientation = CurrentRobotOrientation.FORWARD){
+            move_forward_by_squares(current_row - target_row);
+            move_right_by_squares(target_column - current_column);
+        }
+        else if(currentRobotOrientation = CurrentRobotOrientation.BACKWARD){
+            move_back_by_squares(current_row - target_row);
+            move_left_by_squares(target_column - current_column);
+        }
+        else if(currentRobotOrientation = CurrentRobotOrientation.LEFT){
+            move_right_by_squares(current_row - target_row);
+            move_back_by_squares(target_column - current_row);
+        }
+        else {
+            move_left_by_squares(current_row - target_row);
+            move_forward_by_squares(target_column - current_row);
+        }
         currentSquare = target_square;
     }
 

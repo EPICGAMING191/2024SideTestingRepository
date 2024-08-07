8e6bd36e538c469818737dc87ec47ed501a3ec8d
EPICGAMING191 <BurrJ22@outlook.com>
2024-08-07 18:05:52 -0400

    Started new feature for binding and releasing functions to gamepad keys

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JackBurr/Other/BindFunctionsToGamepadKeys.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.JackBurr.Other;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+public class BindFunctionsToGamepadKeys {
+    public void bind(Object key, Object function){
+        //TODO: Store keys and multiple functions for each key
+    }
+
+    public void release(Object key, Object function){
+        //TODO: Release specific function from specified key
+    }
+
+    public void update_(){
+        //TODO: Check stored keys, if key pressed execute stored function
+    }
+}
